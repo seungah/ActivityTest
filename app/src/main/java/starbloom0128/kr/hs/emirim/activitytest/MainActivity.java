9430bb6ca1ee33d38e286cb90bde8f45cd31649b
seungah <fofo2525@naver.com>
2017-04-13 20:24:33 +0900

    #3 ThirdLayout

--- app/src/main/java/starbloom0128/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/starbloom0128/kr/hs/emirim/activitytest/MainActivity.java
@@ -1,7 +1,10 @@
 package starbloom0128.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button butMain=(Button)findViewById(R.id.but_main);
+        butMain.setOnClickListener(butMainHandler);
     }
+
+    View.OnClickListener butMainHandler=new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
